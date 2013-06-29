38321686cf8fddec289045e95bd77fe012d14f56
ptahmose <ptahmose@live.com>
2013-06-30 01:19:33 +0200

    don't use 'E' or 'I' as the name of a variable because it is defined in mathimf.h which is required with ICL

--- libavcodec/vp8dsp.c
+++ libavcodec/vp8dsp.c
@@ -194,15 +194,15 @@ static av_always_inline int simple_limit(uint8_t *p, ptrdiff_t stride, int flim)
 }
 
 /**
- * E - limit at the macroblock edge
- * I - limit for interior difference
+ * e - limit at the macroblock edge
+ * i - limit for interior difference
  */
-static av_always_inline int normal_limit(uint8_t *p, ptrdiff_t stride, int E, int I)
+static av_always_inline int normal_limit(uint8_t *p, ptrdiff_t stride, int e, int i)
 {
     LOAD_PIXELS
-    return simple_limit(p, stride, E)
-        && FFABS(p3-p2) <= I && FFABS(p2-p1) <= I && FFABS(p1-p0) <= I
-        && FFABS(q3-q2) <= I && FFABS(q2-q1) <= I && FFABS(q1-q0) <= I;
+    return simple_limit(p, stride, e)
+        && FFABS(p3-p2) <= i && FFABS(p2-p1) <= i && FFABS(p1-p0) <= i
+        && FFABS(q3-q2) <= i && FFABS(q2-q1) <= i && FFABS(q1-q0) <= i;
 }
 
 // high edge variance
